17b9437c59fe8d99d9b8eeb826b63b998c0d960e
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-04-04 15:31:52 +0200

    Semi-finished game

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
+let userScore= 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const choices = ['rock', 'paper','scissors'];
     const randomIndex = Math.floor(Math.random()*choices.length);
-    console.log("This is your random number: "+ randomIndex)
+    arrChoice = choices[randomIndex]
+    return arrChoice;
 }
-getComputerChoice()
+    function rockpaperscissors(){
+    let basicSelection = prompt("Enter rock , paper or scissors");
+    console.log(`Computer picked `+ arrChoice + `, you picked ` +basicSelection);
+    console.log(`_____________________________________________________________`)
+    playerSelection = basicSelection;
+    if (playerSelection === arrChoice){
+        console.log('Sorry but it\'s a tie');
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'rock' && arrChoice === 'paper'){
+        console.log('You lost');
+        computerScore = computerScore + 1;
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'rock' && arrChoice === 'scissors'){
+        console.log('You won');
+        userScore = userScore+1;
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'scissors' && arrChoice === 'paper'){
+        console.log('Congrats you won');
+        userScore = userScore+1;
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'scissors' && arrChoice === 'rock'){
+        console.log('You lost');
+        computerScore = computerScore + 1;
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'paper' && arrChoice === 'rock'){
+        console.log('Congrats you won');
+        userScore = userScore+1;
+        console.log(`_____________________________________________________________`)
+    } else if(playerSelection === 'paper' && arrChoice === 'scissors'){
+        console.log('You lost');
+        computerScore = computerScore + 1;
+        console.log(`_____________________________________________________________`)
+    }
+}
 
-function rockpaperscissors(playerSelection, computerSelection){
-    
-}
+getComputerChoice()
+rockpaperscissors()
+getComputerChoice()
+rockpaperscissors()
+getComputerChoice()
+rockpaperscissors()
+getComputerChoice()
+rockpaperscissors()
+console.log(`Your score is `+userScore+`, computer's score is `+ computerScore)
