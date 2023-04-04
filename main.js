64ae9ea9f4aa201eea324549cf1b012e6b7f7300
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-04-04 13:12:56 +0200

    Added random function to js & added core html

---
+++ main.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    const choices = ['rock', 'paper','scissors'];
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    console.log("This is your random number: "+ randomIndex)
+}
+getComputerChoice()
+
+function rockpaperscissors(playerSelection, computerSelection){
+    
+}
